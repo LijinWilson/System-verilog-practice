ed639d63b3ad94b8f12cdf24c189b0ab91be1a56
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-03-15 12:00:27 +0530

    Extern function and one example
    A sample extern function is defined of adding two numbers

---
+++ concepts/Classes/extern_function.sv
@@ -0,0 +1,31 @@
+// ---------------------------------------- EXTERNAL FUNCTION ----------------------------------------
+/*
+- The logic/definition of the function is defined outside the class declaration.
+- SYNTAX: extern virtual void/int/ function function_name();
+- How to define the function outside class?
+      function void class_name::function_name();
+        statements;
+      endfunction
+*/
+/*
+- An external function derived inside the class;
+- Function is basically adding 2 numbers;
+- on main module printing the values of sum;
+*/
+
+class extern_class;
+  extern virtual function int display(int a, int b);
+endclass
+    
+    function int extern_class::display(int a, int b);
+      return a + b;
+    endfunction
+    
+    
+    module extern_function_module();
+      initial
+        begin
+          extern_class ex = new();
+          $display("External Sum = %0d", ex.display(10, 20));
+        end
+    endmodule
