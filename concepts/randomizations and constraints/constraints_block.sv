0f75efcf90f816cb45b7074c16da4f9e15cf0b72
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-03-19 21:46:31 +0530

    added constraints outside class
    - controlling the constraints value

--- concepts/randomizations and constraints/constraints_block.sv
+++ concepts/randomizations and constraints/constraints_block.sv
@@ -21,6 +21,9 @@
 -> Solution: addr will get value more than 5;
 */
 
+
+// ----------------------- CONSTRAINTS DEFINED OUTSIDE CLASS -----------------------
+
 class constraints_rand;
 //   rand will get value from 0 to 15 as it is 3 bit;
   rand bit [3:0] addr;
@@ -52,4 +55,37 @@ endmodule
 # KERNEL: addr = 8
 */
 
+// ----------------------- CONSTRAINTS DEFINED OUTSIDE CLASS -----------------------
+class constraints_rand;
+//   rand will get value from 0 to 15 as it is 3 bit;
+  rand bit [3:0] addr;
+  
+// constraints defined outside class  
+  constraint addr_range;
+  
+endclass
 
+// constraints defined outside class
+  constraint constraints_rand::addr_range {addr > 5;}
+
+module constraints_rand_module();
+  initial
+    begin
+      constraints_rand cr = new();
+      repeat(5)
+        begin
+//  enabling the randomization
+          cr.randomize();
+          $display("addr = %0d", cr.addr);
+        end
+    end
+endmodule
+
+// ----------------------- OUTPUT -----------------------
+/*
+# KERNEL: addr = 15
+# KERNEL: addr = 12
+# KERNEL: addr = 14
+# KERNEL: addr = 8
+# KERNEL: addr = 8
+*/
