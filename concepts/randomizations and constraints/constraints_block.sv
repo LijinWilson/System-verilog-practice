c2567230af91045072b47ff606ffefca33e47d5d
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-03-19 20:33:58 +0530

    added constraints randomization

---
+++ concepts/randomizations and constraints/constraints_block.sv
@@ -0,0 +1,15 @@
+// ----------------------- CONSTRAINED BLOCK -----------------------
+
+/* ----------------------- CONSTRAINED RANDOMIZATION -----------------------
+- It is used in controlling the random value we will get specific value in a range.
+- 
+*/
+
+// ----------------------- CONSTRAINED BLOCKS -----------------------
+/*
+- It looks similar to classes.
+- Unique name inside class.
+- Contains conditions to controls the random value.
+- Enclosed with in braces [].
+- It can defined inside or outside class.     
+*/
