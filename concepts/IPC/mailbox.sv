d16b79a1445238efdda680aa6d729665678112b5
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-03-27 22:42:42 +0530

    added sample example with explanation

--- concepts/IPC/mailbox.sv
+++ concepts/IPC/mailbox.sv
@@ -35,9 +35,63 @@
   -> GET(): Get the message from the mailbox.
 */
 
+//                                 -----------SAMPLE CODE -----------
+module simple_mailbox();
+  mailbox mail;
+  initial
+    begin
+      mail = new();
+      fork
+        sender();
+        reciever();
+      join
+    end
+  //   defining the sender logic;
+  task sender();
+    for(int i = 0; i <= 10; i=i+1)
+      begin
+        #2;
+        $display("At %0t time, sending %0d to mailbox", $time, i);
+        mail.put(i);
+      end
+  endtask
+  
+//   defining the logic of reciever
+  task reciever();
+//     defining the variable for getting data
+    int data;
+    repeat(3)
+      begin
+        mail.get(data);
+        $display("At time %0t recieved %0d", $time, data);
+      end
+  endtask
+  
+  
+  
+endmodule
+/*
+  -> Task and reciver will exicute parallely at same time
+  -> Semder will keep on sending data and reciver will recieve those data at same time.and display those values.
+*/
 
+// --------------------------- OUTPUT -------------------------------
+# KERNEL: At 2 time, sending 0 to mailbox
+# KERNEL: At time 2 recieved 0
+# KERNEL: At 4 time, sending 1 to mailbox
+# KERNEL: At time 4 recieved 1
+# KERNEL: At 6 time, sending 2 to mailbox
+# KERNEL: At time 6 recieved 2
+# KERNEL: At 8 time, sending 3 to mailbox
+# KERNEL: At 10 time, sending 4 to mailbox
+# KERNEL: At 12 time, sending 5 to mailbox
+# KERNEL: At 14 time, sending 6 to mailbox
+# KERNEL: At 16 time, sending 7 to mailbox
+# KERNEL: At 18 time, sending 8 to mailbox
+# KERNEL: At 20 time, sending 9 to mailbox
+# KERNEL: At 22 time, sending 10 to mailbox
 
-
+// ------------------------------------------- EXAMPLE - 2 ------------------------------------------
 
 
 
