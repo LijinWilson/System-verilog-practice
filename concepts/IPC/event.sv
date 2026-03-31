9296b0590002489531f89c163dfb25341e1f46c3
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-03-31 22:43:17 +0530

    Created event.sv, added theoretical concepts of event and wait

---
+++ concepts/IPC/event.sv
@@ -0,0 +1,32 @@
+//         SYSTEM VERILOG EVENT
+/*
+    ->  Maintain synchronization between process.
+    
+    ->  Event operation is of Two staged process.
+          *  One process will trigger the event.
+              => Event can be triggered using -> or ->> operator
+          *  Other process will wait for event to get trigger.
+              => Triggered using wait or @ opeartor.
+    
+    ->  EVENT TRIGGERING.
+        *  Named event are reprsneted using (->)
+        *  Triggering an event unblocks all event that is waiting on that event.
+        *  Non-Blocking events are trigggered using this (->>) operator
+    
+    ->  WAITING FOR EVENT TRIGGER.
+        =>  @ Operator.
+            *  SHORT MEANING: Don't do anything, just wait until something happens.
+            *  @event → wait for something to happen.
+            *  ->event → something just happened.
+            *  You must start waiting (@event) before the event happens (->event)
+    
+        =>  WAIT OPERATOR
+            *  @event → can miss the event if both happen at the same time
+            *  wait(event.triggered) → will not miss it.
+
+        =>  WAIT_ORDER OPERATOR
+            *  wait_order() waits for multiple events to happen in a specific order
+            *  If it not triggered in order it will not unblock the process and cause runtime error.
+    
+        
+*/
