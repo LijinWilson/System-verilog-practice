1dbadb2815575605710f9be3090e568eaba47a91
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-05-11 20:29:37 +0530

    added the functional Coverage

---
+++ concepts/Coverage/coverage.sv
@@ -0,0 +1,20 @@
+// ========================================= COVERAGE ================================================
+
+/*
+    *  It is used to test the tested and untested area in an design.
+    *  It is the percentage of verifications that have met.
+    *  Two Type:
+        *  CODE COVERAGE: Measure how much RTL code is executed (Did code run?..).
+        *  FUNCTIONAL COVERAGE: Measure whether all importand functional things are happened (Did intended scenarios occur?..).
+              * Two Types:
+                    *  DATA ORIENTED COVERAGE:  checks whether all relevant input/output data values and ranges have been used (What values/data were used?..).
+                    *  CONTROL ORIENTED COVERAGE:  Control-oriented coverage checks whether all control paths, states, and sequences of operation have been exercised (How design behaved / which paths taken?..)
+*/
+
+// ============================================= FUNCTIONAL COVERAGE ===================================================
+/*
+  -  Duringe verification we have to measure that all the design behavior, conditions, have been checked/runned.
+  -  Did I verify what the design is supposed to do?
+  -  Even though 100% code coverage achieved, but still can miss the corner case, illegal scenarios, protocol violation etc...
+  -  FUNCTION COVERAGE ensure the VERIFICATION COMPLETNESS.
+*/
